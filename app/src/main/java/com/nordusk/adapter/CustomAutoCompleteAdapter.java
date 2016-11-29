3a9fffe7079f73052a71c98a882921d5ce5b3e2d
amitkp <amitkumar89@hotmail.com>
2016-11-29 11:59:08 +0530

    28_11 add counter distributor field add and autocomplete function

---
+++ app/src/main/java/com/nordusk/adapter/CustomAutoCompleteAdapter.java
@@ -0,0 +1,94 @@
+package com.nordusk.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.TextView;
+
+import com.nordusk.R;
+import com.nordusk.webservices.ParentId;
+
+import java.util.ArrayList;
+
+/**
+ * Created by DELL on 28-11-2016.
+ */
+
+
+public class CustomAutoCompleteAdapter extends ArrayAdapter<ParentId> {
+    private final String MY_DEBUG_TAG = "CustomerAdapter";
+    private ArrayList<ParentId> items;
+    private ArrayList<ParentId> itemsAll;
+    private ArrayList<ParentId> suggestions;
+    private int viewResourceId;
+
+    public CustomAutoCompleteAdapter(Context context, int viewResourceId, ArrayList<ParentId> items) {
+        super(context, viewResourceId, items);
+        this.items = items;
+        this.itemsAll = (ArrayList<ParentId>) items.clone();
+        this.suggestions = new ArrayList<ParentId>();
+        this.viewResourceId = viewResourceId;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        if (v == null) {
+            LayoutInflater vi = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = vi.inflate(viewResourceId, null);
+        }
+        ParentId parentId = items.get(position);
+        if (parentId != null) {
+            TextView customerNameLabel = (TextView) v.findViewById(R.id.customerNameLabel);
+            if (customerNameLabel != null) {
+//              Log.i(MY_DEBUG_TAG, "getView Customer Name:"+customer.getName());
+                customerNameLabel.setText(parentId.getName());
+            }
+        }
+        return v;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return nameFilter;
+    }
+
+    Filter nameFilter = new Filter() {
+        @Override
+        public String convertResultToString(Object resultValue) {
+            String str = ((ParentId)(resultValue)).getId();
+            return str;
+        }
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            if(constraint != null) {
+                suggestions.clear();
+                for (ParentId parentId : itemsAll) {
+                    if(parentId.getName().toLowerCase().startsWith(constraint.toString().toLowerCase())){
+                        suggestions.add(parentId);
+                    }
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = suggestions;
+                filterResults.count = suggestions.size();
+                return filterResults;
+            } else {
+                return new FilterResults();
+            }
+        }
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            ArrayList<ParentId> filteredList = (ArrayList<ParentId>) results.values;
+            if(results != null && results.count > 0) {
+                clear();
+                for (ParentId c : filteredList) {
+                    add(c);
+                }
+                notifyDataSetChanged();
+            }
+        }
+    };
+
+}
---
+++ app/src/main/java/com/nordusk/webservices/ParentIdAsync.java
@@ -0,0 +1,133 @@
+package com.nordusk.webservices;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.text.TextUtils;
+
+import com.nordusk.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by DELL on 28-11-2016.
+ */
+public class ParentIdAsync extends AsyncTask<Void, Void, Void> {
+
+    private Activity context;
+    private boolean isTimeOut = false;
+
+    private String responsecode = "";
+    private String responseMessage = "";
+    private ProgressDialog mpProgressDialog;
+    private JSONObject jsonObject = null;
+
+    private ArrayList<ParentId> arrayList = new ArrayList<ParentId>();
+
+
+    public ParentIdAsync(Activity context) {
+        this.context = context;
+        mpProgressDialog = new ProgressDialog(context);
+        mpProgressDialog.setMessage("Loading..");
+        mpProgressDialog.show();
+
+
+        mpProgressDialog.setCancelable(false);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        try {
+
+
+            String[] responsedata = HttpConnectionUrl.post(context, context.getResources().getString(R.string.base_url) + "distributer_name_suggest.php?", jsonObject);
+            isTimeOut = (!TextUtils.isEmpty(responsedata[0]) && responsedata[0].equals(HttpConnectionUrl.RESPONSECODE_REQUESTSUCCESS)) ? false : true;
+            if (!isTimeOut && !TextUtils.isEmpty(responsedata[1])) {
+                parseResponseData(responsedata[1]);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        if (mpProgressDialog != null && mpProgressDialog.isShowing())
+            mpProgressDialog.dismiss();
+        if (isTimeOut) {
+            if (onContentListParserListner != null) {
+                onContentListParserListner.OnConnectTimeout();
+            }
+        }
+        if (onContentListParserListner != null && responsecode.equalsIgnoreCase("200")) {
+            onContentListParserListner.OnSuccess(arrayList);
+
+        } else {
+            if (responseMessage != null && responseMessage.length() > 0) {
+                onContentListParserListner.OnError(responseMessage);
+            } else {
+                HttpConnectionUrl.serverErrorMessage(context, responsecode);
+            }
+        }
+    }
+
+    private void parseResponseData(String response) {
+        JSONObject jsonObject;
+        try {
+            jsonObject = new JSONObject(response);
+            responsecode = jsonObject.getString("response_code");
+            if (jsonObject != null && responsecode.equalsIgnoreCase("200")) {
+                JSONArray jsonArray = jsonObject.getJSONArray("list");
+                if (jsonArray != null && jsonArray.length() > 0) {
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject object = jsonArray.getJSONObject(i);
+                        ParentId list = new ParentId();
+                        list.setId(HttpConnectionUrl.getJSONKeyvalue(object, "id"));
+                        list.setName(HttpConnectionUrl.getJSONKeyvalue(object, "name"));
+
+                        arrayList.add(list);
+
+                    }
+                }
+
+
+            } else {
+                responseMessage = "No parentid found";
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private OnContentListSchedules onContentListParserListner;
+
+    public OnContentListSchedules getOnContentListParserListner() {
+        return onContentListParserListner;
+    }
+
+    public void setOnContentListParserListner(OnContentListSchedules onContentListParserListner) {
+        this.onContentListParserListner = onContentListParserListner;
+    }
+
+    public interface OnContentListSchedules {
+        public void OnSuccess(ArrayList<ParentId> arrayList);
+
+        public void OnError(String str_err);
+
+        public void OnConnectTimeout();
+    }
+}
